3deae274aa5677122041c4ba07f5a775bde2b432
yjk01 <yxk19a@acu.edu>
2021-02-11 21:05:31 -0600

    FORREAL

--- Box/src/main/java/Box.java
+++ Box/src/main/java/Box.java
@@ -3,15 +3,15 @@ public class Box
 {
 	public String box1()
 	{
-		return "M 20.0 10.0 v -5.0 h 20.0 v 5.0 h -20.0";
+		return "M 20.0 10.0 v-5.0 h 20.0 v5.0 h-20.0";
 	}
 	public String box2()
 	{
-		return "M 20.0 10.0 v 10.0 h 20.0 v -10.0";
+		return "M 20.0 10.0 v10.0 h20.0 v-10.0";
 	}
 	public String box3()
 	{
-		return "M 20.0 10.0 h-5.0 v10.0 h 5.0";
+		return "M 20.0 10.0 h-5.0 v10.0 h5.0";
 	}
 	public String box4()
     {
@@ -25,18 +25,18 @@ public class Box
 
     public String box6()
     {
-        return "M 45.0 10.0 h20.0 v10.0 hgit ad-20.0";
+        return "M 45.0 10.0 h20.0 v10.0 h-20.0";
     }
 
 	
     public static void main( String[] args )
     {
-        System.out.println( "Box 1: M 20.0 10.0 v -5.0 h 20.0 v 5.0 h -20.0" );
-		System.out.println( "Box 2: M 20.0 10.0 v 10.0 h 20.0 v -10.0");
-		System.out.println( "Box 3: M 20.0 10.0 h-5.0 v10.0 h 5.0");
-		System.out.println( "Box 4: M 20.0 20.0 v5.0 h20.0 v-5.0");
-		System.out.println(" Box 5: M 40.0 10.0 h5.0 v10 h-5.0");
-		System.out.println(" Box 6: M 45.0 10.0 h20.0 v10.0 hgit ad-20.0";)
+        System.out.println("Box 1: M 20.0 10.0 v-5.0 h20.0 v5.0 h-20.0");
+		System.out.println("Box 2: M 20.0 10.0 v10.0 h20.0 v-10.0");
+		System.out.println("Box 3: M 20.0 10.0 h-5.0 v10.0 h 5.0");
+		System.out.println("Box 4: M 20.0 20.0 v5.0 h20.0 v-5.0");
+		System.out.println("Box 5: M 40.0 10.0 h5.0 v10 h-5.0");
+		System.out.println("Box 6: M 45.0 10.0 h20.0 v10.0 h-20.0");
     }
 	
 }
