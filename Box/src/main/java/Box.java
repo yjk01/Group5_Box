7174d3b710af33eee0468e840eaacb5eeda5be59
yjk01 <yxk19a@acu.edu>
2021-02-11 21:16:30 -0600

    YJ COMMIT

--- Box/src/main/java/Box.java
+++ Box/src/main/java/Box.java
@@ -5,7 +5,7 @@ public class Box
 	{
 		return "M 20.0 10.0 v-5.0 h 20.0 v5.0 h-20.0";
 	}
-	
+
 	public String box2()
 	{
 		return "M 20.0 10.0 v10.0 h20.0 v-10.0";
@@ -36,7 +36,7 @@ public class Box
     {
         System.out.println("Box 1: M 20.0 10.0 v-5.0 h20.0 v5.0 h-20.0");
 		System.out.println("Box 2: M 20.0 10.0 v10.0 h20.0 v-10.0");
-		System.out.println("Box 3: M 20.0 10.0 h-5.0 v10.0 h 5.0");
+		System.out.println("Box 3: M 20.0 10.0 h-5.0 v10.0 h5.0");
 		System.out.println("Box 4: M 20.0 20.0 v5.0 h20.0 v-5.0");
 		System.out.println("Box 5: M 40.0 10.0 h5.0 v10 h-5.0");
 		System.out.println("Box 6: M 45.0 10.0 h20.0 v10.0 h-20.0");
