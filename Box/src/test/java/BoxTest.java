f5dd0e17ed7efc433b130eaa538b6c6da1c65f0e
Julio Lopez <65047730+julio254@users.noreply.github.com>
2021-02-11 21:22:42 -0600

    Update BoxTest.java

--- Box/src/test/java/BoxTest.java
+++ Box/src/test/java/BoxTest.java
@@ -1,36 +1,87 @@
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.*;
 
 /**
- * Unit test for simple App.
+ *     Group 5
+ *     Box Test Cases
  */
-public class AppTest 
-    extends TestCase
+public class BoxTest 
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+
+    Box box_test;
+
+    @Before
+    public void initialize() {
+        box_test = new Box();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void Box1() {
+        try {
+            string box_one = "Box 1: M 20.0 10.0 v -5.0 h 20.0 v 5.0 h -20.0";
+            string expect = box_test.Box1();
+            assertEquals(box_one, expect);
+        } catch (Exception e) {
+            System.out.println("Box1 catch");
+        }
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void Box2() {
+        try {
+            string box_one = "Box 2: M 20.0 10.0 v 10.0 h 20.0 v -10.0";
+            string expect = box_test.Box2();
+            assertEquals(box_one, expect);
+        } catch (Exception e) {
+            System.out.println("Box2 catch");
+        }
+    }
+
+    @Test
+    public void Box3() {
+        try {
+            string box_one = "Box 3: M 20.0 10.0 h-5.0 v10.0 h 5.0";
+            string expect = box_test.Box3();
+            assertEquals(box_one, expect);
+        } catch (Exception e) {
+            System.out.println("Box3 catch");
+        }
+    }
+
+    @Test
+    public void Box4() {
+        try {
+            string box_one = "Box 4: M 20.0 20.0 v5.0 h20.0 v-5.0";
+            string expect = box_test.Box4();
+            assertEquals(box_one, expect);
+        } catch (Exception e) {
+            System.out.println("Box4 catch");
+        }
+    }
+
+    @Test
+    public void Box5() {
+        try {
+            string box_one = "Box 5: M 40.0 10.0 h5.0 v10.0 h-5.0";
+            string expect = box_test.Box5();
+            assertEquals(box_one, expect);
+        } catch (Exception e) {
+            System.out.println("Box5 catch");
+        }
+    }
+
+    @Test
+    public void Box6() {
+        try {
+            string box_one = "Box 6: M 45.0 10.0 h20.0 v10.0 hgit ad-20.0";
+            string expect = box_test.Box6();
+            assertEquals(box_one, expect);
+        } catch (Exception e) {
+            System.out.println("Box6 catch");
+        }
     }
 }
