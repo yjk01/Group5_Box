d79bb90896bf37e2a49811e34a9d3103e7090f8c
Julio Lopez <Juliol2540@gmail.com>
2021-03-04 14:54:38 -0600

    updated tests

--- Box/src/test/java/BoxTest.java
+++ Box/src/test/java/BoxTest.java
@@ -87,69 +87,32 @@ public class BoxTest
             System.out.println("Box6 catch");
         }
     }
+    
+    /*
+     
+     Unit Tests ---------------------------------------------------------------------------------------------------
+     
+     */
+    
     @Test
-    public void Box7() {
-        try {
-            String box_one = "M 20.0 10.0 v-5.0 h20.0 v5.0 h-20.0";
-            String expect = box_test.box1();
-            assertEquals(box_one, expect);
-        } catch (Exception e) {
-            System.out.println("Box1 catch");
-        }
+    public void UnitTest1() {
+        String svg = "M 20.0 10.0 v-5.0 h20.0";
+        int length = svg.indexOf("v");
+        if (length < 0) {
+                System.out.println("Vertical line not found");
+        } else {
+                System.out.println("Vertical line at index " + length);
+            }
     }
-
+    
     @Test
-    public void Box8() {
-        try {
-            String box_one = "M 20.0 10.0 v10.0 h20.0 v-10.0";
-            String expect = box_test.box2();
-            assertEquals(box_one, expect);
-        } catch (Exception e) {
-            System.out.println("Box2 catch");
-        }
-    }
-
-    @Test
-    public void Box9() {
-        try {
-            String box_one = "M 20.0 10.0 h-5.0 v10.0 h5.0";
-            String expect = box_test.box3();
-            assertEquals(box_one, expect);
-        } catch (Exception e) {
-            System.out.println("Box3 catch");
-        }
-    }
-
-    @Test
-    public void Box10() {
-        try {
-            String box_one = "M 20.0 20.0 v5.0 h20.0 v-5.0";
-            String expect = box_test.box4();
-            assertEquals(box_one, expect);
-        } catch (Exception e) {
-            System.out.println("Box4 catch");
-        }
-    }
-
-    @Test
-    public void Box11() {
-        try {
-            String box_one = "M 40.0 10.0 h5.0 v10 h-5.0";
-            String expect = box_test.box5();
-            assertEquals(box_one, expect);
-        } catch (Exception e) {
-            System.out.println("Box5 catch");
-        }
-    }
-
-    @Test
-    public void Box12() {
-        try {
-            String box_one = "M 45.0 10.0 h20.0 v10.0 h-20.0";
-            String expect = box_test.box6();
-            assertEquals(box_one, expect);
-        } catch (Exception e) {
-            System.out.println("Box6 catch");
-        }
+    public void UnitTest2() {
+        String svg = "M 20.0 10.0 v-5.0 h20.0";
+        int width = svg.indexOf("h");
+        if (width < 0) {
+                System.out.println("Horizontal line not found");
+        } else {
+                System.out.println("Horizontal line at index " + width);
+            }
     }
 }
