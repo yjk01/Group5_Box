950501a5e5ce937a79bfe67c1a6f4fdcdfba6353
yjk01 <yxk19a@acu.edu>
2021-03-18 21:28:03 -0500

    new test cases - YJ

--- Box/src/test/java/BoxTest.java
+++ Box/src/test/java/BoxTest.java
@@ -93,9 +93,12 @@ public class BoxTest
      Unit Tests ---------------------------------------------------------------------------------------------------
      
      */
-    
+
     @Test
     public void UnitTest1() {
+
+        System.out.println();
+
         String svg = "M 20.0 10.0 v-5.0 h20.0";
         int length = svg.indexOf("v");
         if (length < 0) {
@@ -115,4 +118,120 @@ public class BoxTest
                 System.out.println("Horizontal line at index " + width);
             }
     }
+
+    @Test
+    public void UnitTest3()
+    {
+        System.out.println();
+
+        String str = "M20 10.0 v-5.0 h20.0 v5.0 h-20.0";
+        String[] spt = str.split(" ", 0);
+
+        System.out.println("Vertical lines found with heights of: ");
+
+        for(int i = 0; i < spt.length; i++)
+        {
+            if(spt[i].charAt(0) == 'v')
+                System.out.println(spt[i]);
+        }
+    }
+
+    @Test
+    public void UnitTest4()
+    {
+        System.out.println();
+
+        String str = "M20 10.0 v10.0 h20.0 v-10.0 h-20.0";
+        String[] spt = str.split(" ", 0);
+
+        System.out.println("Vertical lines found with heights of: ");
+        
+        for(int i = 0; i < spt.length; i++)
+        {
+            if(spt[i].charAt(0) == 'v')
+                System.out.println(spt[i]);
+        }
+    }
+
+    @Test
+    public void UnitTest5()
+    {
+        System.out.println();
+
+        String str = "M20 10.0 v-5.0 h20.0 v5.0 h-20.0";
+        String[] spt = str.split(" ", 0);
+
+        System.out.println("Horizontal lines found with lengths of: ");
+        
+        for(int i = 0; i < spt.length; i++)
+        {
+            if(spt[i].charAt(0) == 'h')
+            {
+                for(int j = 1; j < spt[i].length(); j++)
+                {
+                    System.out.print(spt[i].charAt(j));
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    @Test
+    public void UnitTest6()
+    {
+        System.out.println();
+
+        String str = "M40.0 10.0 h5.0 v10.0 h-5.0";
+        String[] spt = str.split(" ", 0);
+
+        System.out.println("Horizontal lines found with lengths of: ");
+        
+        for(int i = 0; i < spt.length; i++)
+        {
+            if(spt[i].charAt(0) == 'h')
+            {
+                for(int j = 1; j < spt[i].length(); j++)
+                {
+                    System.out.print(spt[i].charAt(j));
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    // test for uniformity of lengths
+    @Test
+    public void UnitTest7()
+    {
+        System.out.println();
+
+        String str = "M20 10.0 v-5.0 h20.0 v5.0 h-20.0";
+        String[] spt = str.split(" ", 0);
+        String s;
+
+        double arr[] = new double[10];
+        
+        for(int i = 0; i < spt.length; i++)
+        {
+            if(spt[i].charAt(0) == 'h')
+            {
+                 s = spt[i].replace("h", "");
+
+                arr[i] = Double.parseDouble(s);
+            }
+        }
+
+        double sum = 0;
+
+        for(int i = 0; i < arr.length; i++)
+            sum += arr[i];
+
+        if(sum == 0.0)
+            System.out.println("All horizontal lines are the same length");
+
+        else
+            System.out.println("Fix Horizonatal lines");
+    }
+
+
 }
