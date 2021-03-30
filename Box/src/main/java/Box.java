a18807333e828191615715cd5ce3c61b1d980e20
Julio Lopez <Juliol2540@gmail.com>
2021-03-30 14:42:51 -0500

    basic input

--- Box/src/main/java/Box.java
+++ Box/src/main/java/Box.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Box 
 {
@@ -31,6 +34,31 @@ public class Box
         return "M 45.0 10.0 h20.0 v10.0 h-20.0";
     }
 
+	public static void makeSVG() {
+		// Make File
+		try {
+			File myObj = new File("svgtest.svg");
+			if(myObj.createNewFile()) {
+				System.out.println("File created: " + myObj.getName());
+			} else {
+				System.out.println("File already exists.");
+			}
+		}	catch(IOException e) {
+				System.out.println("An error occured.");
+				e.printStackTrace();
+		}
+
+		// Write to file
+		try {
+      		FileWriter myWriter = new FileWriter("svgtest.svg");
+      		myWriter.write("This wrote to the file");
+      		myWriter.close();
+      		System.out.println("Successfully wrote to the file.");
+    	} catch (IOException e) {
+      		System.out.println("An error occurred.");
+      		e.printStackTrace();
+    	}
+	}
 	
     public static void main( String[] args )
     {
@@ -40,6 +68,8 @@ public class Box
 		System.out.println("Box 4: M 20.0 20.0 v5.0 h20.0 v-5.0");
 		System.out.println("Box 5: M 40.0 10.0 h5.0 v10 h-5.0");
 		System.out.println("Box 6: M 45.0 10.0 h20.0 v10.0 h-20.0");
+
+		makeSVG();
     }
 	
 }
